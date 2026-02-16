7561afd8cf7ac66d1e644439b2040409639f336f
daveryderoxford <dave@theryderclan.co.uk>
2026-02-16 09:53:03 +0000

    Indicator for lazy loaded modules

--- sailbrowser-web/src/app/app.ts
+++ sailbrowser-web/src/app/app.ts
@@ -1,22 +1,34 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal, viewChild } from '@angular/core';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
-import { RouterOutlet } from "@angular/router";
+import { RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterOutlet } from "@angular/router";
 import { LazyInject } from './shared/services/lazy-injector';
 import { SidenavService } from './shared/services/sidenav.service';
 import { SidenavMenu } from "./sidenav-menu/sidenav-menu";
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs/operators';
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: 'app.scss',
-  imports: [MatSidenavModule, RouterOutlet, SidenavMenu],
+  imports: [MatSidenavModule, RouterOutlet, SidenavMenu, MatProgressBarModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App implements OnInit {
   sidebarService = inject(SidenavService);
   private lazyInject = inject(LazyInject);
+  private router = inject(Router);
 
   sidenav = viewChild.required(MatSidenav);
+
+  protected isLazyLoading = toSignal(
+    this.router.events.pipe(
+      filter(e => e instanceof RouteConfigLoadStart || e instanceof RouteConfigLoadEnd),
+      map(e => e instanceof RouteConfigLoadStart)
+    ),
+    { initialValue: false }
+  );
 
   ngOnInit() {
     this.sidebarService.setSidenav(this.sidenav());
